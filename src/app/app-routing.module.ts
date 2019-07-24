076ea57cf672d9adbb3f6b04829792d4a0ffecc7
RAU <stephan.rauh@opitz-consulting.com>
2019-07-24 18:47:43 +0200

    collected a list of useful operators

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import { MapOperatorComponent } from './map-operator/map-operator.component';
 import { ForkJoinOperatorComponent } from './fork-join-operator/fork-join-operator.component';
 
 const routes: Routes = [
+  { path: 'map-js', component: MapOperatorComponent },
   { path: 'map', component: MapOperatorComponent },
   { path: 'forkjoin', component: ForkJoinOperatorComponent },
+  { path: 'forkjoin-chaining', component: ForkJoinOperatorComponent },
   { path: '', redirectTo: '/map', pathMatch: 'prefix' }
 ];
 
